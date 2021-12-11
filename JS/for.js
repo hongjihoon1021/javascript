6ca1847e55a217c57bc40b43481f456c7fbd7114
jyhoon <hongjihoon1021@gmail.com>
2021-12-11 18:19:29 +0900

    반복문

---
+++ JS/for.js
@@ -0,0 +1,9 @@
+// 1부터 10까지 짝수만 출력하기
+// for (let i =2; i<=10; i+=2){
+//     console.log(i-1)
+// }
+for (let i=0; i<10; i++){
+    if ((i+1)%2===0){
+        console.log(i+1);
+    }
+}
---
+++ JS/while.js
@@ -0,0 +1,5 @@
+let num=1
+while (num<=5){
+    num=num+1
+    console.log("안녕")
+}
